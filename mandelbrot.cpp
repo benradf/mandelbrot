fc0a122da02b6f05ff37c8c53ba58d2494839d2b
Ben Radford <benradf@users.noreply.github.com>
2023-04-30 15:38:38 +0100

    WIP

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -1,8 +1,11 @@
 #include <cassert>
 #include <complex>
+#include <cstdint>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <tuple>
+#include <vector>
 
 using namespace std;
 
@@ -25,6 +28,81 @@ tuple<int, int, int> hsl(double h, double s, double l)
     return make_tuple(g(0), g(8), g(4));
 }
 
+
+#pragma pack(push, 1)
+
+struct FileHeader {
+    uint16_t magic;
+    uint32_t size;
+    uint16_t reserved1;
+    uint16_t reserved2;
+    uint32_t offset;
+};
+
+struct InfoHeader {
+    uint32_t size;
+    int32_t width;
+    int32_t height;
+    uint16_t planes;
+    uint16_t depth;
+    uint32_t compression;
+    uint32_t imageSize;
+    uint32_t horizontalRes;
+    uint32_t verticalRes;
+    uint32_t paletteSize;
+    uint32_t importantColours;
+};
+
+#pragma pack(pop)
+
+template<typename Pixel>
+void writeBitmap(const char* filename, int width, int height, const vector<Pixel>& pixels)
+{
+    FileHeader fileHeader;
+    fileHeader.magic = 0x4D42;
+    fileHeader.size = sizeof(fileHeader) + sizeof(InfoHeader);
+    fileHeader.reserved1 = 0;
+    fileHeader.reserved2 = 0;
+    fileHeader.offset = fileHeader.size;
+
+    InfoHeader infoHeader;
+    infoHeader.size = sizeof(infoHeader);
+    infoHeader.width = width;
+    infoHeader.height = height;
+    infoHeader.planes = 1;
+    infoHeader.depth = 24;
+    infoHeader.compression = 0;
+    infoHeader.horizontalRes = 1;
+    infoHeader.verticalRes = 1;
+    infoHeader.paletteSize = 0;
+    infoHeader.importantColours = 0;
+
+    uint32_t unpaddedWidth = (infoHeader.depth >> 3) * width;
+    uint32_t paddedWidth = ((unpaddedWidth - 1) & 0xFFFFFFFC) + 4;
+    uint32_t padBytes = paddedWidth - unpaddedWidth;
+
+    infoHeader.imageSize = paddedWidth * height;
+    fileHeader.size += infoHeader.imageSize;
+
+    std::ofstream file(filename, std::ios::binary);
+    assert(file.is_open());
+
+    file.write(reinterpret_cast<char*>(&fileHeader), sizeof(fileHeader));
+    file.write(reinterpret_cast<char*>(&infoHeader), sizeof(infoHeader));
+
+    for (int32_t y = height - 1; y >= 0; y--) {
+        for (uint32_t x = 0; x < width; x++) {
+            auto pixel = pixels.at(y * width + x);
+            file.write(reinterpret_cast<char*>(&pixel), 3);
+        }
+        for (uint32_t i = 0; i < padBytes; i++) 
+            file.put(0);
+    }
+
+    file.close();
+}
+
+
 int main(int argc, char* argv[])
 {
     const auto MAX_K = 500;
@@ -41,11 +119,16 @@ int main(int argc, char* argv[])
     auto scale = atoi(argv[2]);
     auto kmult = atoi(argv[3]);
 
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size * 2; ++j) {
+    auto width = size * 2;
+    auto height = size;
+
+    vector<tuple<uint8_t, uint8_t, uint8_t>> pixels(width * height);
+
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
             auto c = complex(
-                    OFFSET_X + (double(j) - size) / double(scale) / 2.0,
-                    OFFSET_Y + (double(i) - size / 2.0) / double(scale)
+                    OFFSET_X + (double(j) - width / 2.0) / double(scale) / 2.0,
+                    OFFSET_Y + (double(i) - height / 2.0) / double(scale)
             );
             auto x = complex(0.0, 0.0);
             int k = 0;
@@ -66,6 +149,17 @@ int main(int argc, char* argv[])
         }
         cout << endl;
     }
+
+    pixels.emplace_back(255, 0, 0);
+    pixels.emplace_back(0, 255, 0);
+    pixels.emplace_back(0, 0, 255);
+    pixels.emplace_back(255, 255, 255);
+    writeBitmap("mandelbrot.bmp", 2, 2, pixels);
+
+    //for (int i = 0; i < pixels.size() * 3; ++i) {
+    //    auto p = reinterpret_cast<const uint8_t*>(pixels.data());
+    //    printf("%04x: %02x\n", i, p[i]);
+    //}
 
     return 0;
 }
